f16fb97765294f053edfe42fc942200953012eca
@revakovsky <revakovsky@gmail.com>
2022-05-26 15:18:27 +0300

    added retrofit to the project

---
+++ app/src/main/java/com/example/myapplication2/Support.kt
@@ -0,0 +1,6 @@
+package com.example.myapplication2
+
+data class Support(
+    val text: String,
+    val url: String
+)
---
+++ app/src/main/java/com/example/myapplication2/TestingDataClass.kt
@@ -0,0 +1,6 @@
+package com.example.myapplication2
+
+data class TestingDataClass(
+    val `data`: Data,
+    val support: Support
+)
