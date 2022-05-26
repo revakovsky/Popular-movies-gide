4435b14469ae7ec28c1c5fae6ead594cf93d1263
Maksym Revakovskyi <94849698+revakovsky@users.noreply.github.com>
2022-05-26 14:26:54 +0300

    Revert "Adding retrofit"

--- app/src/main/java/com/example/myapplication2/Movie.kt
+++
@@ -1,3 +0,0 @@
-package com.example.myapplication2
-
-data class Movie(var title: String, var image: String)
--- app/src/main/java/com/example/myapplication2/Support.kt
+++
@@ -1,6 +0,0 @@
-package com.example.myapplication2
-
-data class Support(
-    val text: String,
-    val url: String
-)
